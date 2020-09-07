aa6db89fb246cda51f74ae363234538af2afa18e
Mubarak Aldjaidi <mubarak_aldjaidi@hotmail.com>
2020-09-07 11:17:41 +0200

    taakaf

--- 2-Select/taak04/antwoorden.sql
+++ 2-Select/taak04/antwoorden.sql
@@ -1,9 +1,9 @@
 -- Opdracht 1 
-
+SELECT Max(wage) As hoogste_loon_speler_fc_utrecht FROM players
 -- Opdracht 2 
-
+SELECT AVG(wage) As gemiddelde_loon_alle_spelers FROM players
 -- Opdracht 3
-
+SELECT SUM(wage) As som_speler_fc_groningen FROM players
 -- Opdracht 4
 
 -- Opdracht 5
